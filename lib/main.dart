d841e4f2c87c445dcb35fae45361a9c470c9b24c
Hassank455 <kamalhassan538@gmail.com>
2021-06-29 00:49:59 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'AR Face Filters App',
       home: MySplashScreen(),
     );
